181cdba2fd747d83506c67b122908b85a2303225
X <bakedbrokenbread@gmail.com>
2025-09-10 18:22:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,3 +22,24 @@ client.on('messageCreate', message => {
 });
 
 client.login(process.env.TOKEN);
+
+
+
+
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('Pong! 🃏');
+  }
+});
+
+client.login(process.env.TOKEN);
+
+
